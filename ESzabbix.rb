5dcc52b5dcf64ea07923237b426fa6e2360cfe50
Denis Barishev <denz@twiket.com>
2014-02-28 16:02:29 +0700

    new monitoring script and templates

--- ESzabbix.rb
+++ ESzabbix.rb
@@ -4,6 +4,7 @@ require 'elasticsearch'
 require 'json'
 
 class ESStats
+  METRIC_GROUPS = ['nodes_stats', 'nodes_info', 'health']
   attr_reader :es_read_timeout, :cache_timeout, :metrics, :cache_file
 
   def initialize
@@ -12,42 +13,21 @@ class ESStats
     @cache_file    = '/tmp/eszabbix_stats.cache'
     @cache_timeout = 60
     @node_id       = nil
+    @statistics    = nil
   end
 
-  # Read es statistics, cache opearation.
-  def stats
-    @stats ||= begin
-      mtime = File.mtime(cache_file) rescue (Time.now - cache_timeout)
-
-      # load statistics from elasticsearch
-      if Time.now - mtime > cache_timeout
-        hash  = {node_id: '_local'}
-        metrics.inject(hash) {|h, v| h[v.to_sym] = true; h}
-        node_info = client.nodes.info(hash)
-        node_id   = node_info['nodes'].keys.first
-
-        # read indices stats for all cluster nodes
-        stats = client.nodes.stats metric: 'indices'
-        stats['nodes'][node_id].merge!(node_info['nodes'][node_id])
-
-        # cache results
-        File.open(cache_file, 'w') {|f| f.write(JSON.pretty_generate(stats))}
-        stats
-      else
-        # from cache
-        JSON.parse(IO.read(cache_file))
-      end
+  METRIC_GROUPS.each do |meth|
+    define_method(meth) do
+      read_statistics
+      statistics["#{meth}"]
     end
   end
 
+  # retrieve node_id from nodes_info, since nodes_info contains data only for local node
   def node_id
     @node_id ||= begin
-      # node_id becomes available after stats has been read
-      # local node is the only one which has extended metrics
-      idx = stats['nodes'].find_index do |id, hash|
-        hash.keys.any? {|k| metrics.include? k}
-      end
-      stats['nodes'].keys[idx]
+      read_statistics
+      statistics['nodes_info']['nodes'].keys.first
     end
   end
 
@@ -60,93 +40,103 @@ class ESStats
       c
     end
   end
+
+  private
+
+  attr_reader :statistics
+
+  def read_statistics
+    @statistics ||= begin
+      all = {}
+      mtime = File.mtime(cache_file) rescue (Time.now - cache_timeout)
+
+      # load statistics from elasticsearch
+      if Time.now - mtime > cache_timeout
+        # Load local nodes info for all specified metrics
+        info_opts = @metrics.inject({}) {|h, m| h[m.to_sym] = true; h}
+        node_info = client.nodes.info(info_opts.merge({node_id: '_local'}))
+        @node_id ||= node_info['nodes'].keys.first
+        all['nodes_info'] = node_info
+        all['health'] = client.cluster.health
+
+        # Read indices metrics cluster-wide
+        all['nodes_stats'] = client.nodes.stats metric: 'indices'
+
+        # Separately for each metric load node local stats, should work on both 0.90, 1.x
+        @metrics.inject(all['nodes_stats']) do |hash, m|
+          data = client.nodes.stats({node_id: '_local', metric: m})
+          hash['nodes'][node_id].merge!(data['nodes'][node_id])
+          hash
+        end
+
+        File.open(cache_file, 'w') {|f| f.write(JSON.pretty_generate(all))}
+        all
+      else
+        # from cache
+        JSON.parse(IO.read(cache_file))
+      end
+    end
+  end
 end
 
 class ESZabbix
-  attr_reader :es, :path, :item
+  attr_reader :es, :metric_group, :metric_path, :cluster
 
   def initialize
     @es = ESStats.new
-    @mode = nil
-    @modes_supported = :cluster, :service
-    @service_items = ['ping']
-    @cluster_items = ['health']
+    @cluster = false
   end
 
-  def metric(item)
-    parse_item(item)
-    case mode
-    when :service
-      service_value
-    when :cluster
-      cluster_value
+  def metric(args)
+    parse_args(args)
+    if metric_group == 'service'
+      zbx_fail "Unknown path #{metric_path}. Supports only service ping." if metric_path != 'ping'
+      begin
+        es.client.ping ? 1 : 0
+      rescue TimeoutError
+        0
+      end
+    elsif ESStats::METRIC_GROUPS.include? metric_group
+      if !metric_path.start_with?('indices') && cluster
+        zbx_fail "Aggregation is only available for indices metrics. Use cluster: prefix"
+      elsif cluster
+        es.nodes_stats['nodes'].keys.inject(0) do |s, id|
+          s = s + read_metric_from(es.nodes_stats['nodes'][id])
+        end
+      else
+        metrics = es.send(metric_group.to_sym)
+        if metric_group == 'health' && metric_path == 'status'
+          # Report status as green in case of failure, the item should be
+          # aggregated over a cluster to provide cluster status.
+          ['green','yellow','red'].find_index {|s| es.health['status'] = s} rescue 0
+        elsif metric_group == 'health'
+          read_metric_from(metrics)
+        else
+          read_metric_from(metrics['nodes'][es.node_id])
+        end
+      end
     else
-      # get value directly from node statistics
-      direct_value_from(es.stats['nodes'][es.node_id])
+      zbx_fail "Unknown metric group #{metric_group}"
     end
-  rescue Timeout::Error
-    zbx_fail "Couldn't connect to ElasticSearch opearation timed out"
-  rescue Exception => e
-    zbx_fail e.message + "\n" + e.backtrace.join("\n")
+  rescue SystemExit
+    raise
   end
 
   private
 
-  attr_reader :mode
-
-  # retrieve cluster metric value
-  def cluster_value
-    case path
-    when 'health'
-      stats = es.client.cluster.health
-      # 0, 1, 2 for green, yellow, red respectively
-      ['green', 'yellow', 'red'].find_index {|i| i == stats['status']}
-    else
-      unless path.start_with?('indices')
-        zbx_fail "Cluster aggregation is only available for indices metrics"
-      end
-      es.stats['nodes'].inject(0) do |value, pair|
-        node_stats = pair.last
-        value = value + direct_value_from(node_stats)
-      end
-    end
-  rescue Timeout::Error
-    # In case of timeout health is reported as GREEN! The actual cluster health
-    # must be aggregated in zabbix to avoid useless triggering for each node.
-    path == 'health' ? 0 : raise
+  def read_metric_from(hash)
+    metric_path.split('.').inject(hash) {|h, i| h[i]} or zbx_fail "Could not read path #{metric_path} for #{metric_group}"
+  rescue
+    zbx_fail "Could not read path #{metric_path} for #{metric_group}"
   end
 
-  # retrieve cluster metric value
-  def service_value
-    case path
-    when 'ping'
-      es.client.ping ? 1 : 0
-    else
-      zbx_fail("Unknown service item #{path}")
-    end
-  rescue Timeout::Error
-    0
-  end
-
-  def direct_value_from(hash)
-    value = path.split('.').inject(hash) {|h, i| h[i]}
-    value or zbx_fail "Value for item #{path} is empty"
-  end
-
-  # parse item like "(cluster:)indices.docs.count"
-  def parse_item(item)
-    @item = item
-    zbx_fail "You must provide zabbix statistics path like: (cluster:)indices.docs.count" if item.nil?
-    parsed = item.split(/(\w+):/)
-    case parsed[1]
-    when nil
-      @path = parsed[0].to_s
-    when 'cluster', 'service'
-      @mode = parsed[1].to_sym
-      @path = parsed[2].to_s
-    else
-      zbx_fail "Unknown opearation mode #{parsed[1]}"
-    end
+  # parse and check arguments
+  def parse_args(args)
+    zbx_fail 'You must provide two arguments like "nodes_stats indices.docs.count"' if args.size < 2
+    @metric_group, @metric_path = args
+    list = @metric_path.split(':')
+    @metric_path = list.last
+    @cluster = true if list.size > 1 && list.first == 'cluster'
   end
 
   # zabbix fail with unsupported item message
@@ -158,4 +148,4 @@ class ESZabbix
 end
 
 stats = ESZabbix.new
-puts stats.metric(ARGV[0])
+puts stats.metric(ARGV)
